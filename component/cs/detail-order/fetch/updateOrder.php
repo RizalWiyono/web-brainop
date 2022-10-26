e92181bcc617ca7e070356f49c408a8f406ea6be
Muhammad Rizal Wiyono <69446968+RizalWiyono@users.noreply.github.com>
2022-10-26 09:17:42 +0700

    Update updateOrder.php

--- component/cs/detail-order/fetch/updateOrder.php
+++ component/cs/detail-order/fetch/updateOrder.php
@@ -8,12 +8,13 @@ $adding_price = $_POST["adding_price"];
 $newPice= str_replace(".", "", $price);
 
 $total = $newPice + $adding_price;
+$newTotal = number_format($total);
 
 mysqli_query($connect, "UPDATE detailorder SET status='Unpaid' WHERE iddetailOrder=$idParam");
 
 mysqli_query($connect, "INSERT INTO transaction 
 ( idtransaction, iddetailOrder, price_order, proof_of_payment, status) 
 values 
-(null, $idParam, '$total', '-', 'Unpaid')");
+(null, $idParam, '$newTotal', '-', 'Unpaid')");
 
 header("location: ../?pr=unpaid");
